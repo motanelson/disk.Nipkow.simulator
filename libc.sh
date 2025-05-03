3a44c0cdaa3aa1197cdbde454df4f2659a4c6c5e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-03 21:33:43 +0100

    Delete libc.sh

--- libc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh 
-printf "\033c\033[43;30m\ndownloa gcc"
-wget "https://ftp.gnu.org/gnu/glibc/glibc-2.41.tar.gz" 
-gzip -d 'glibc-2.41.tar.gz'
-tar --extract -f 'glibc-2.41.tar'
-cd glibc-2.41
- 
