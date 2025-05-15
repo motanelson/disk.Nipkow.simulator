37de20da0a55b33296ea27fe9c33230033b0c78e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-15 10:27:23 +0100

    Add files via upload

--- disk.py
+++ disk.py
@@ -25,7 +25,6 @@ class NipkowDiskApp:
     def draw_disk(self):
         self.canvas.delete("all")
         z=0
-        
         # Desenhar disco principal
         self.canvas.create_oval(
             self.center - self.radius, self.center - self.radius,
@@ -34,10 +33,15 @@ class NipkowDiskApp:
         )
 
         for i in range(self.num_holes):
-            z=z+4
+            z=z+7
+            
             angle_deg = (360 / self.num_holes) * i + self.angle_offset
+            if angle_deg>360.00:
+                angle_deg=angle_deg-360.00
             angle_rad = math.radians(angle_deg)
 
+            
+            
             # Coordenadas do final da linha (na borda do disco)
             x = self.center + math.cos(angle_rad) * (self.radius- z - 10)
             y = self.center + math.sin(angle_rad) * (self.radius -z- 10)
@@ -47,11 +51,15 @@ class NipkowDiskApp:
 
             # Círculo no final da linha (ponto)
             r = 6
-            color = "red" if i == self.current_index else "black"
+            color= "black"
             self.canvas.create_oval(
                 x - r, y - r, x + r, y + r,
                 fill=color
             )
+             
+            color = "red" 
+            if angle_deg>(36*6) and angle_deg<(36*7):
+                self.canvas.create_oval(x - r, y - r, x + r, y + r,fill=color)
 
     def animate(self):
         self.angle_offset = (self.angle_offset + 5) % 360
